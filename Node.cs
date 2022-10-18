42c3e37c6293b0c8c4b4b3e755d51450b518d4e8
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-10-18 15:10:47 +0530

    Add files via upload
    Added UC3

---
+++ Node.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserCase3AddAtEnd
+{
+        class Node
+        {
+            public int data;
+            public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+
+        }
+
+        class LinkedList
+        {
+            Node head;
+
+            public LinkedList()
+            {
+                head = null;
+            }
+       }
+
+    }
+
+
+
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace UserCase3AddAtEnd
+{
+    internal class program
+    {
+        public static void Main()
+        {
+            UC3AddAtEnd List = new UC3AddAtEnd();
+            List.AddAtEnd(56);
+            List.AddAtEnd(30);
+            List.AddAtEnd(70);
+            List.Display();
+            Console.ReadKey();
+        }
+    }
+}
