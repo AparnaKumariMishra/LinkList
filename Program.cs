1cac02957543b68f94103ae3c229284278851754
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-10-18 15:09:37 +0530

    Add files via upload
    Added UC2

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Usercase2AddAt1stLocation
+{
+    internal class program
+    {
+        public static void Main()
+        {
+            UC2AddAt1st list = new UC2AddAt1st();
+            //list.Add(56);
+            //list.Add(30);
+            //list.Add(70);
+           // list.AddAll(23, 56, 78, 34, 56);
+            list.InsertAtFirstPosition(1,70);
+            list.InsertAtFirstPosition(1,30);
+            list.InsertAtFirstPosition(1,56);
+            list.Display();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UC2AddAt1st.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Usercase2AddAt1stLocation
+{
+    public class UC2AddAt1st
+    {
+        internal Node head; //new 
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next; //temp=null
+            }
+
+        }
+        internal Node InsertAtFirstPosition(int position, int data)
+        {
+            if (position < 1)
+                Console.WriteLine("Invalid position");
+            if (position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+
+
+                    if (position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                    }
+                    temp = temp.next;//1000
+                if (position != 1)
+                    Console.WriteLine("Position out of range");
+            }
+            return head;
+
+        }
+    }
+}
